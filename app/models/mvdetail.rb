d4d42b2bb6d9b4e21ca7a633e4dc4511545dbbbb
horstwurm <horst.wurm@bluewin.ch>
2017-11-16 19:14:34 +0000

    mvdetails

--- app/models/mvdetail.rb
+++ app/models/mvdetail.rb
@@ -1,7 +1,5 @@
 class Mvdetail < ApplicationRecord
     belongs_to :mobject
     
-has_attached_file :video,
-    styles: lambda { |a| a.instance.is_image? ? {:small => "x200>", :medium => "x300>", :large => "x400>"}  : {:thumb => { :geometry => "100x100#", :format => 'jpg', :time => 10}, :medium => { :geometry => "300x300#", :format => 'jpg', :time => 10}}},
-    processors: lambda { |a| a.is_video? ? [ :ffmpeg ] : [ :thumbnail ] }
+has_attached_file :video
 end
