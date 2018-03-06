cab2529c2b25068957afc1f319f7c854f2311224
horstwurm <horst.wurm@bluewin.ch>
2018-03-06 15:37:19 +0000

    init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,10 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery3
+//= require jquery 
+//= require jquery_ujs
 //= require popper
+//= require turbolinks
 //= require bootstrap
 //= require bootstrap-sprockets
 //= require bootstrap-datepicker
