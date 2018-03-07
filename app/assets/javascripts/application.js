ae1aafd9c0146fffddb289c13361959b29aec146
horstwurm <horst.wurm@bluewin.ch>
2018-03-07 15:15:49 +0000

    init

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery 
+//= require jquery.min
 //= require jquery_ujs
 //= require turbolinks
 //= require popper
@@ -20,3 +20,5 @@
 //= require bootstrap-slider
 //= require owl.carousel
 //= require bxslider
+//= require bootstrap-switch
+
